fec17d562d71ce59a9c4c73573fa4859a1de7812
xiaoze <1416065820@qq.com>
2020-12-17 13:29:31 +0800

    使用express写接口

--- node.js-day-04/10.使用express写接口.js
+++ node.js-day-04/10.使用express写接口.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+
 const router = require('./11.apirouter')
     //app.use注册全局中间键
 app.use('/api', router)
+
+//对post请求进行配置
+router.use(express.urlencoded({ extended: false }))
+
 app.listen('80', () => {
     console.log('express server running');
 })
